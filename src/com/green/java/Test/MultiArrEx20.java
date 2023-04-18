b2a7d8d3bce488e1465f24f882146fc2798305a9
Hack <dlwlsrb0307@naver.com>
2023-04-18 19:55:01 +0900

    third

---
+++ src/com/green/java/Test/MultiArrEx20.java
@@ -0,0 +1,7 @@
+package com.green.java.Test;
+
+public class MultiArrEx20 {
+    public static void main(String[] args) {
+
+    }
+}
--- src/com/green/java/ch05/ArrayEx19_2.java
+++ src/com/green/java/ch05/ArrayEx19_2.java
@@ -14,16 +14,16 @@ package com.green.java.ch05;
 
 public class ArrayEx19_2 {
     public static void main(String[] args) {
-        String[] titles ={ "국어", "영어", "수학", "사회"};
+        String[] titles ={ "국어", "영어", "수학"};
 
         int sum2 = 0;
         int sum = 0;
         double avg = 0;
         int [] [] score = {
-                {100, 90, 80, 50},  //국어
-                {20, 20, 20, 50},
-                {30, 30, 30, 50},
-                {40, 40, 40, 50}
+                {100, 90, 80},  //국어
+                {20, 20, 20},
+                {30, 30, 30},
+                {40, 40, 40}
 
         };
         System.out.println("번호 국어 영어 수학 총점 평균");
@@ -31,23 +31,23 @@ public class ArrayEx19_2 {
 
         for (int i = 0; i < score.length; i++) {
             sum = 0;
-            System.out.printf("%d \t", i+1);
+            System.out.printf("%d\t",i+1);
+
             for (int j = 0; j < score[i].length; j++) {
                 System.out.printf("%d\t",score[i][j]);
-                sum += score[i][j];
-                avg = (double)sum /score[i].length;
+                sum +=score[i][j];
+                avg = (double) sum /score[i].length;
             }
-            System.out.printf("%2d %.2f\n",sum,avg);
-        }
-        System.out.println("=========================================");
-        System.out.println("총합");
-        for (int i = 0; i < titles.length; i++) {
-            sum2 = 0;
-            for (int j = 0; j < score.length; j++) {
-                sum2 += score[j][i];
-            }System.out.println(sum2);
+            System.out.printf("%d %.2f \n",sum,avg);
         }
 
-        
+        System.out.println("===============================");
+        for (int i = 0; i < titles.length; i++) {
+            sum = 0;
+            for (int j = 0; j < score.length; j++) {
+                sum +=score[j][i];
+            }
+            System.out.println(titles[i]+ ":" + sum);
+        }
     }
 }
